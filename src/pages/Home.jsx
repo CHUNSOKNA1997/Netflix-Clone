be0f8840ace6748e3d1a86337de214fd03b08617
CHUNSOKNA1997 <chun.sokna.1222@gmail.com>
2025-03-06 12:00:38 +0700

    added Play and More Info buttons in Home.jsx

--- src/pages/Home.jsx
+++ src/pages/Home.jsx
@@ -1,6 +1,8 @@
 import React from "react";
 import Navbar from "../components/Navbar";
 import hero from "../assets/hero_title.png";
+import { Info, Play } from "lucide-react";
+
 const Home = () => {
   return (
     <main className="relative background-banner min-h-screen w-full">
@@ -11,6 +13,16 @@ const Home = () => {
           modern instanbul embarks on a quest to save a city from an immortal
           enemy.
         </p>
+        <div className="flex gap-5">
+          <button className="bg-white flex items-center justify-center px-7 py-3 rounded-md text-black gap-2 hover:cursor-pointer hover:bg-gray-700">
+            <Play size={24} />
+            <span>Play</span>
+          </button>
+          <button className="bg-white-opacity flex items-center justify-center px-7 py-3 rounded-md text-white gap-2">
+            <Info size={24} />
+            <span>More Info</span>
+          </button>
+        </div>
       </div>
       <Navbar />
     </main>
