4ff082d71bde1ba3cac20f8fe635b8282115667f
CHUNSOKNA1997 <chun.sokna.1222@gmail.com>
2025-03-07 20:44:21 +0700

    added trailer link and movie's information to home page of Hero Banner

--- src/pages/Home.jsx
+++ src/pages/Home.jsx
@@ -12,19 +12,25 @@ const Home = () => {
       <div className="pt-32 px-15 text-white space-y-7 w-1/3">
         <img src={hero} alt="hero_title" className="w-full max-w-lg" />
         <p>
-          Discovering his ties to a secret ancient order, a young man living in
-          modern Istanbul embarks on a quest to save a city from an immortal
-          enemy.
+          Hundreds of cash-strapped players accept a strange invitation to
+          compete in children's games. Inside, a tempting prize awaits — with
+          deadly high stakes.
         </p>
         <div className="flex gap-5">
-          <button className="bg-white flex items-center justify-center px-7 py-3 rounded-md text-black gap-2 hover:cursor-pointer hover:bg-gray-300 transition-colors duration-300">
+          <a
+            href="https://www.youtube.com/watch?v=lQBmZBJCYcY"
+            className="bg-white flex items-center justify-center px-7 py-3 rounded-md text-black gap-2 hover:cursor-pointer hover:bg-gray-300 transition-colors duration-300"
+          >
             <Play size={24} />
             <span>Play</span>
-          </button>
-          <button className="bg-gray-700 bg-opacity-70 flex items-center justify-center px-7 py-3 rounded-md text-white gap-2 hover:cursor-pointer hover:bg-gray-500 transition-colors duration-300">
+          </a>
+          <a
+            href="https://www.netflix.com/tudum/articles/squid-game-season-2-confirmed"
+            className="bg-gray-700 bg-opacity-70 flex items-center justify-center px-7 py-3 rounded-md text-white gap-2 hover:cursor-pointer hover:bg-gray-500 transition-colors duration-300"
+          >
             <Info size={24} />
             <span>More Info</span>
-          </button>
+          </a>
         </div>
       </div>
 
