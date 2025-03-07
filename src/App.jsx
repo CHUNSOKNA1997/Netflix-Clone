abf2da9f485a687cd7ceb614005d259c20ff358e
CHUNSOKNA1997 <chun.sokna.1222@gmail.com>
2025-03-07 13:02:10 +0700

    install react router and react router dom

--- src/App.jsx
+++ src/App.jsx
@@ -1,10 +1,15 @@
 import React from "react";
 import Home from "./pages/Home";
+import Login from "./pages/Login";
+import { Routes, Route } from "react-router-dom";
 
 const App = () => {
   return (
     <>
-      <Home />
+      <Routes>
+        <Route path="/" element={<Home />} />
+        <Route path="/login" element={<Login />} />
+      </Routes>
     </>
   );
 };
