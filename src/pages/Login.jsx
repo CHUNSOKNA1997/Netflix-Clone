621f36e28cde3ea083390ea80b697f92fb2ebe5a
CHUNSOKNA1997 <chun.sokna.1222@gmail.com>
2025-03-09 12:17:27 +0700

    completly finish login page both sign in and sign up

--- src/pages/Login.jsx
+++ src/pages/Login.jsx
@@ -1,7 +1,9 @@
-import React from "react";
+import React, { useState } from "react";
 import logo from "../assets/logo.png";
 
 const Login = () => {
+  const [signState, setSignStateSet] = useState("Sign In");
+
   return (
     <div className="relative h-screen w-full background-banner-login">
       <div className="absolute top-10 left-32 z-10">
@@ -10,9 +12,23 @@ const Login = () => {
 
       <div className="relative z-10 flex items-center justify-center min-h-screen px-4">
         <div className="w-full max-w-md p-8 py-16 background-login-form rounded-xl">
-          <h1 className="mb-6 text-[2rem] text-white font-bold">Sign In</h1>
+          <h1 className="mb-6 text-[2rem] text-white font-bold">{signState}</h1>
 
           <form>
+            {signState === "Sign Up" ? (
+              <div className="mb-4">
+                <div className="relative">
+                  <input
+                    type="text"
+                    className="w-full p-4 background-input-field text-white rounded focus:outline-none"
+                    placeholder="Username"
+                  />
+                </div>
+              </div>
+            ) : (
+              <></>
+            )}
+
             <div className="mb-4">
               <div className="relative">
                 <input
@@ -50,13 +66,33 @@ const Login = () => {
             </div>
           </div>
 
-          <div className="mt-4 text-base text-gray-400">
-            New to Netflix?{" "}
-            <a href="#" className="text-white hover:underline">
-              Sign up now
-            </a>
-            .
-          </div>
+          {signState === "Sign In" ? (
+            <div className="mt-4 text-base text-gray-400">
+              New to Netflix?{" "}
+              <a
+                className="text-white hover:underline hover:cursor-pointer"
+                onClick={() => {
+                  setSignStateSet("Sign Up");
+                }}
+              >
+                Sign Up now
+              </a>
+              .
+            </div>
+          ) : (
+            <div className="mt-4 text-base text-gray-400">
+              Already have an account?{" "}
+              <a
+                className="text-white hover:underline hover:cursor-pointer"
+                onClick={() => {
+                  setSignStateSet("Sign In");
+                }}
+              >
+                Sign In now
+              </a>
+              .
+            </div>
+          )}
 
           <div className="mt-4 text-xs text-gray-500">
             This page is protected by Google reCAPTCHA to ensure you're not a
